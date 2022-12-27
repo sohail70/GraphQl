d272da1861a11e845bc7375ec1b1373ee8d9432f
sohail70 <sohail.e.nia@gmail.com>
2022-12-27 20:31:15 +0330

    Add ability to UI to create a customer using useMutation from the apollo grphql client : I actually forgot to do it in previous session so there are some lines of code that are irrelavant

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
-import {useQuery , gql} from '@apollo/client';
-import { useEffect } from 'react';
+import {useQuery, useMutation , gql} from '@apollo/client';
+import { useEffect , useState } from 'react';
 
 
 // export type Launch =  {
@@ -22,12 +22,19 @@ import { useEffect } from 'react';
 //     }
 //   }`;
 
+export type Order ={
+  id: number;
+  description: string;
+  totalInCents: number;
+  
+}
 
 
 export type Customer =  {
         id: number;
         name: string;
         industry: string;
+        orders: Order[];
     };
 
 const GET_DATA = 
@@ -37,12 +44,35 @@ gql`
       id
       name
       industry
+      orders{
+        id
+        description
+        totalInCents
+      }
     }
   }`;
+
+  // deghat kun String ro ba S bozor benvisi dar paeen
+const MUTATE_DATA = gql`
+  mutation MUTATE_DATA($name: String! , $industry: String!){ 
+    createCustomer(name: $name , industry: $industry){
+      customer{
+        id
+        name
+      }
+    }
+  }
+`; //gql`` means its a graph query language string
 function  App() {
+  const [name , setName] = useState<string>(''); // age empty string bezari  default ro on vaght dar form nizai nist hatman name ro por kuni
+  const [industry , setIndustry] = useState<string>(''); // meghdar default komak mikune ke age input ro dar UI por nakardi va mostaghim button e Add customer ro zadi gir nade behet
+
   const {loading , error , data} = useQuery(GET_DATA);
+  const [createCustomer , {loading: createCustomerLoading , error: createCustomerError , data: createCustomerData}] = useMutation(MUTATE_DATA , {refetchQueries:[{query: GET_DATA}]}); // mittoni jaye esme MUTATE_DATA benvisi add_csutomer --> kulan harchi dost dari benvis
+
   useEffect(()=>{
     console.log(loading , error , data);
+    console.log(createCustomerLoading , createCustomerError , createCustomerData); //log kunim bebinim chi tosh hast!
   });
 
 
@@ -55,9 +85,53 @@ function  App() {
       {error ? <p>Sth wend wring</p>:null}
       {loading? <p>loading...</p>: null}
       {data ? data.customers.map((customer: Customer)=>{
-          return <p key={customer.id}>{customer.name + ' ' + customer.industry}</p>
+          return (
+            <div>
+              <h2 key={customer.id}>{customer.name + ' (' + customer.industry + ')'}</h2>
+              {customer.orders.map((order: Order)=>{
+                return( 
+                <div>
+                  <p>{order.description}</p>
+                  <p>Cost: ${(order.totalInCents/100).toLocaleString(undefined , {minimumFractionDigits: 2 , maximumFractionDigits: 2})}</p>
+                </div>
+                );
+              })}
+            </div>
+          )
         }):null}
+
+
+
+        <form onSubmit={(e)=>{
+          e.preventDefault(); // Prevent a page refresh after submit
+          // console.log('submitting' , name , industry);
+          createCustomer({ variables: {name: name , industry: industry}}) //name va industry dovom state haye ma hastan
+          if(!error){
+            setName('');
+            setIndustry('');
+          }
+        }}>
+          <div>
+              <label htmlFor="name">Name:</label>
+              <input id="name" type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
+          </div>
+
+          <div>
+              <label htmlFor="industry">Industry:</label>
+              <input id="industry" type="text" value={industry} onChange={(e)=>{setIndustry(e.target.value)}}/>
+          </div>
+
+          <button disabled={createCustomerLoading ? true : false}>Add Customer</button>
+          {createCustomerError ? <p>Error Creating Customer</p> : null}
+
+        </form>
+
+
     </div>
+
+
+
+
   );
 }
 
